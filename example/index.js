a1857e29ede9da20b250d63d1dbfe76df3ad4b01
Daniel Arthur Gallagher <8526031+danieleloscozzese@users.noreply.github.com>
2025-03-26 17:33:48 +0000

    feat: use the new certificate

--- example/index.js
+++ example/index.js
@@ -1,20 +1,20 @@
-'use strict';
+"use strict";
 
 // Note that this example code was designed to be run directly from this
 // folder, some values may have to be changed if you copy and paste this
 // code into your own project.
 
-const path = require('path');
-const bankid = require('../lib/index');
+const path = require("path");
+const bankid = require("../lib/index");
 
-function handleError (err) {
+function handleError(err) {
   console.log(err);
 }
 
 const options = {
-  pfx: path.resolve('../certs/FPTestcert2_20150818_102329.pfx'),
-  passphrase: 'qwerty123',
-  env: 'test',
+  pfx: path.resolve(__dirname + "/" + "../certs/FPTestcert5_20240610.p12"),
+  passphrase: "qwerty123",
+  env: "test",
 };
 
 // This example shows how to use the bankid module to create a new service instance that
@@ -22,7 +22,7 @@ const options = {
 bankid(options, (err, service) => {
   if (err) return handleError(err);
 
-  service.authenticate({}, (err, response) => {
+  service.authenticate({ endUserIp: "127.0.0.1" }, (err, response) => {
     if (err) return handleError(err);
     console.log(response);
   });
